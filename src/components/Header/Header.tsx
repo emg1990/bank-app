6e6e19ebb1954be9697e1433b06dcf4565638572
emg1990 <emg1990gmail.com>
2024-06-04 02:39:14 +0200

    Migrate Menu children to items

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,29 +1,37 @@
 import React from 'react';
 import { Avatar, Menu } from 'antd';
 import { LogoutOutlined, UserOutlined } from '@ant-design/icons';
+import { ItemType, MenuItemType } from 'antd/es/menu/interface';
 import styles from './Header.module.css';
 
-const Header = () => {
+const Header: React.FC = () => {
+  const items: ItemType<MenuItemType>[] = [
+    {
+      key: 'user',
+      label: (
+        <div className={styles.userMenu}>
+          <Avatar icon={<UserOutlined />} />
+          <span>John Doe</span>
+        </div>
+      ),
+      children: [
+        {
+          key: 'account',
+          icon: <UserOutlined />,
+          label: 'Account',
+        },
+        {
+          key: 'logout',
+          icon: <LogoutOutlined />,
+          label: 'Logout',
+        },
+      ],
+    },
+  ];
   return (
     <div className={styles.header}>
       <div className={styles.logo}>logo</div>
-      <Menu mode='vertical' className={styles.menu}>
-        <Menu.SubMenu
-          title={
-            <div className={styles.userMenu}>
-              <Avatar icon={<UserOutlined />} />
-              <span>John Doe</span>
-            </div>
-          }
-        >
-          <Menu.Item key="account">
-            <UserOutlined /> Account
-          </Menu.Item>
-          <Menu.Item key="logout">
-            <LogoutOutlined /> Logout
-          </Menu.Item>
-        </Menu.SubMenu>
-      </Menu>
+      <Menu mode='vertical' className={styles.menu} items={items}/>
     </div>
   );
 };
