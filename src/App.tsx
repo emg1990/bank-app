2b0a18185a1ec14e42269d632e61fe4bff5c1850
emg1990 <emg1990gmail.com>
2024-06-04 02:42:35 +0200

    removing Filler component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
-import Filler from './components/Filler';
 import NewTransfer from './components/NewTransfer/NewTransfer';
 import Home from './components/Home/Home';
 import styles from './App.module.css';
@@ -13,7 +12,7 @@ const App: React.FC = () => {
       <div className={styles.content}>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/:id" element={<Filler />} />
+          <Route path="/:id" element={<>Account</>} />
           <Route path="/create-account" element={<>Create</>} />
           <Route path="/edit-account/:id" element={<>Edit/Delete</>} />
         </Routes>
