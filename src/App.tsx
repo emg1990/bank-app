ef1ece7426d5d2bfaa0de93f4bcd238c61c21765
emg1990 <emg1990gmail.com>
2024-06-08 05:46:26 +0200

    Implementing app context

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,21 @@ import Header from './components/Header/Header';
 import NewTransfer from './components/NewTransfer/NewTransfer';
 import Home from './components/Home/Home';
 import styles from './App.module.css';
+import { AppProvider } from './contexts/AppContext';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Header />
-      <div className={styles.content}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
-      <NewTransfer />
-    </Router>
+    <AppProvider>
+      <Router>
+        <Header />
+        <div className={styles.content}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+        <NewTransfer />
+      </Router>
+    </AppProvider>
   );
 };
 
---
+++ src/contexts/AppContext.tsx
@@ -0,0 +1,64 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { ICurrency, IOwner } from "../types/types";
+import { getCurrencies } from "../api/currenciesApi";
+import { message } from "antd";
+import { getOwner } from "../api/ownersApi";
+
+// Define interface for context
+interface IAppContext {
+  owner: IOwner;
+  currencies: ICurrency[];
+}
+
+const AppContext = createContext<IAppContext | null>(null);
+
+export const AppProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
+  const [currencies, setCurrencies] = useState<ICurrency[]>([]);
+  const [owner, setOwner] = useState<IOwner>({} as IOwner);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  // Fetch currencies and owners from API or any other source
+  useEffect(() => {
+    const fetchCurrencies = async () => {
+      try {
+        const currencies = await getCurrencies();
+        setCurrencies(currencies);
+        message.success("Great success");
+      } catch (error) {
+        // TODO retry if some type of managable error
+        message.error('Failed to fetch currencies');
+        console.error('Failed to fetch currencies:', error);
+      }
+    };
+    const fetchOwner = async () => {
+      try {
+        // TODO get owner from login
+        const owner = await getOwner(1);
+        setOwner(owner);
+      } catch (error) {
+        // TODO retry if some type of managable error
+        message.error('Failed to fetch owner details');
+        console.error('Failed to fetch owner details:', error);
+      }
+    };
+    Promise.all([fetchCurrencies(), fetchOwner()])
+    .then(() => {
+      setLoading(false);
+    });
+  }, []);
+  console.log(loading, owner, currencies);
+
+  return (
+    <AppContext.Provider value={{ currencies, owner }}>
+      {!loading && children}
+    </AppContext.Provider>
+  );
+};
+
+export const useAppContext = () => {
+  const context = useContext(AppContext);
+  if (!context) {
+    throw new Error('useAppContext must be used within an AppProvider');
+  }
+  return context;
+};
