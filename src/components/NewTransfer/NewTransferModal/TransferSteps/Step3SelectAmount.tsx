bf409a354e448def2a242e766b4451c4c505f12f
emg1990 <emg1990gmail.com>
2024-06-11 22:28:06 +0200

    Adding comments on validations

--- src/components/NewTransfer/NewTransferModal/TransferSteps/Step3SelectAmount.tsx
+++ src/components/NewTransfer/NewTransferModal/TransferSteps/Step3SelectAmount.tsx
@@ -41,14 +41,14 @@ const Step3SelectAmount: React.FC<Step3SelectAmountProps> = ({ form, onOk, onBac
   return (
     <Card title="Amount">
       <Form.Item name="amount" label="Amount" rules={[
-        { required: true, message: 'Please enter the transfer amount.' },
+        { required: true, message: 'Please enter the transfer amount.' }, // This rule ensures that the amount is not empty
         () => ({
           validator(_, value) {
-            if (value && value <= 0) {
+            if (value && value <= 0) { // This rule ensures that the amount is greater than 0
               return Promise.reject(new Error('The amount must be greater than 0.'));
             }
             const tolerance = 0.0001; // Define a tolerance value to avoid floating point errors
-            if (value && value > tolerance + balance) {
+            if (value && value > tolerance + balance) { // This rule ensures that the amount is less than the balance
               return Promise.reject(new Error('The amount exceeds the current balance!'));
             }
             return Promise.resolve();
